5c06997b77d0894b9f4b1c675ea4c16704a9d4ab
Edward Chan <edwardchan@Edwards-MacBook-Pro.local>
2017-04-08 12:28:19 -0700

    Adds semicolon per Paul

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 var app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../react-client/dist'));
 
 app.listen(3000, function() {
