27ca11c76306d3049549600c8a0e5aba84902ca7
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2017-04-10 08:15:04 +0100

    Update init.js
    do the database scripts right ;)

--- db/init/init.js
+++ db/init/init.js
@@ -4,71 +4,37 @@ const config = {
   database: 'gecko'
 };
 
-var QueryFile = require('pg-promise').QueryFile;
 var pgp = require('pg-promise')();
-
-function sql(file) {
-  return new QueryFile(file, {minify: true});
-}
-
-var sqlDrop = sql('./tables/dropTables.sql');
-var sqlUsers = sql('./tables/users.sql');
-var sqlTopics = sql('./tables/topics.sql');
-var sqlTags = sql('./tables/tags.sql');
-var sqlTagsTopics = sql('./tables/tags_topics.sql');
-var sqlVotes = sql('./tables/votes.sql');
-var sqlCollab = sql('./tables/collaborators.sql');
-var sqlComments = sql('./tables/comments.sql');
-var sqlFavorites = sql('./tables/favorites.sql');
-var sqlFollows = sql('./tables/follows.sql');
-var sqlMessages = sql('./tables/messages.sql');
-var sqlRatings = sql('./tables/ratings.sql');
-
 var db = pgp(config);
 
-db.query(sqlDrop)
-.then(
-  results => {
-    return db.query(sqlUsers);
-  }
-).then(
-  results => {
-    return db.query(sqlTopics);
-  }
-).then(
-  results => {
-    return db.query(sqlComments);
-  }
-).then(
-  results => {
-    return db.query(sqlCollab);
-  }
-).then(
-  results => {
-    return db.query(sqlFavorites);
-  }
-).then( 
-  results => {
-    return db.query(sqlFollows);
-  }
-).then(
-  results => {
-    return db.query(sqlMessages);
-  }
-).then(
-  results => {
-    return db.query(sqlRatings);
-  }
-).then(
-  results => {
-    return db.query(sqlTags);
-  }
-).then(
-  results => {
-    return db.query(sqlTagsTopics);
-  }
-).then(
-  results => {
-    return db.query(sqlVotes);
-  }
-)
+function sql(file) {
+  var fullPath = path.join(__dirname, './tables', file);
+  return new pgp.QueryFile(fullPath, {minify: true});
+}
+
+var queries = {
+  drop: sql('dropTables.sql'),
+  users: sql('users.sql'),
+  topics: sql('topics.sql'),
+  comments: sql('comments.sql'),
+  collab: sql('collaborators.sql'),
+  favorites: sql('favorites.sql'),
+  follows: sql('follows.sql'),
+  messages: sql('messages.sql'),
+  ratings: sql('ratings.sql'),
+  tags: sql('tags.sql'),  
+  tagsTopics: sql('tags_topics.sql'),
+  votes: sql('votes.sql')  
+};
+
+db.tx(t => {
+  return t.batch(Object.keys(queries).map(k => queries[k]));
+})
+  .then(() => {
+     console.log('SUCCESS');
+     pgp.end(); // to avoid delay exiting the process;
+  })
+  .catch(error => {
+     console.log('ERROR:', error);
+     pgp.end(); // to avoid delay exiting the process;
+  });
