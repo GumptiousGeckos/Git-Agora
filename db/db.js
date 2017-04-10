67c0f77ed6a4f88581af388844f1d3fbbe2fc25f
Paul Mills <paulmills@tests-MacBook-Pro-3.local>
2017-04-10 15:33:32 -0700

    (clean) Remove config comments

--- db/db.js
+++ db/db.js
@@ -1,9 +1,4 @@
 require('dotenv').config()
-// const config = {
-//   host: 'localhost',
-//   port: 5432,
-//   database: 'gecko'
-// };
 
 var config = process.env.DATABASE_URL || process.env.DB_LOCAL;
 
