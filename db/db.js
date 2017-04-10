14ce747724cc2ba2d21be296bef5f51d33212311
Paul Mills <paulmills@tests-MacBook-Pro-3.local>
2017-04-10 15:20:04 -0700

    (feat) Update config variables to use process.env variables

--- db/db.js
+++ db/db.js
@@ -1,4 +1,12 @@
-var config = require('./dbconfig.js');
+require('dotenv').config()
+// const config = {
+//   host: 'localhost',
+//   port: 5432,
+//   database: 'gecko'
+// };
+
+var config = process.env.DATABASE_URL || process.env.DB_LOCAL;
+
 var pgp = require('pg-promise')();
 
 var db = pgp(config);
--- db/init/init.js
+++ db/init/init.js
@@ -1,8 +1,11 @@
-const config = {
-  host: 'localhost',
-  port: 5432,
-  database: 'gecko'
-};
+require('dotenv').config()
+// const config = {
+//   host: 'localhost',
+//   port: 5432,
+//   database: 'gecko'
+// };
+
+var config = process.env.DATABASE_URL || process.env.DB_LOCAL;
 
 var pgp = require('pg-promise')();
 var db = pgp(config);
