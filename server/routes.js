82c2cfdc28e54402e8d1e933e0d50a672a7333f5
Augustus Brennan <augustusmb@gmail.com>
2017-04-10 15:13:47 -0700

    Add express server routes file and basic routes

---
+++ server/routes.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+
+
+
+router.get('/comments', )
+router.post('/comments', )
+
+router.get('/messages', )
+router.post('/messages', )
+router.put('/messages', )
+
+router.get('/votes', )
+router.put('/votes', )
+
+router.get('/users', )
+
+router.get('/user', )
+router.post('/user', )
+router.put('/user', )
+router.delete('/user', )
+
+router.get('/topics', )
+router.post('/topics', )
+router.delete('/topics', )
+
+router.get('/rating', )
+
+router.get('/collaborators', )
+router.post('/collaborators', )
+
+router.get('/favorites', )
+router.put('/favorites', )
+
+router.get('/topics/tags', )
+router.get('/topics', )
+
+router.get('/followers', )
+router.post('/followers', )
+router.put('/followers', )
+router.delete('/followers', )
