f09bba5add7a701109cb99e5aae2e3f4e568e5ac
Edward Chan <edwardchan@Edwards-MacBook-Pro.local>
2017-04-12 10:34:14 -0700

    Adds semicolons

--- server/auth/github.js
+++ server/auth/github.js
@@ -6,7 +6,7 @@ const db = require('../../db/db');
 
 const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT;
 const GITHUB_CLIENT_SECRET = process.env.GITHUB_SECRET;
-const GITHUB_CALLBACK = process.env.GITHUB_CALLBACK
+const GITHUB_CALLBACK = process.env.GITHUB_CALLBACK;
 
 passport.use(new GitHubStrategy({
     clientID: GITHUB_CLIENT_ID,
@@ -16,10 +16,10 @@ passport.use(new GitHubStrategy({
   (accessToken, refreshToken, profile, done) => {
     db.any('INSERT INTO users(id, username, email) SELECT ${id}, ${username}, ${email} WHERE NOT EXISTS (SELECT 1 FROM users WHERE id=${id})', {id: profile.id, username: profile.username, email: profile.email} )
     .then( results => {
-      return done(null, profile)
+      return done(null, profile);
     })
     .catch( error => {
-      return done(error, null)
+      return done(error, null);
     })
   }
 ));
