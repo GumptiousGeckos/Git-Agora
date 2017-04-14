6d36b7e77e44f325483db1c35db16dbba5b1b840
Augustus Brennan <augustusmb@gmail.com>
2017-04-13 21:48:29 -0700

    Add route to add tags

--- server/routes/Tags.js
+++ server/routes/Tags.js
@@ -1,1 +1,28 @@
-Tags.js
+const db = require('./../../db/db.js');
+const path = require('path');
+const pg = require('pg');
+const pgp = require('pg-promise')();
+
+
+
+function sql(file) {
+  var fullPath = path.join(__dirname, './../../db/queries/tags', file);
+  return new pgp.QueryFile(fullPath, {minify: true});
+}
+
+let queries = {
+  addTag: sql('insertTag.sql')
+}
+
+
+module.exports.addTag = (req, res) => {
+  const { tag_name } = req.body;
+
+  return db.query(queries.addTag, [tag_name])
+  .then( () => {
+    res.status(202).send('Success adding tag');
+  })
+  .catch( error => {
+    res.status(404).send('error adding tag');
+  })
+}
