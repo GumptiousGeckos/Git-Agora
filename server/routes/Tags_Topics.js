41a855bd7c4e0f516023d3222f224b5367cb23cd
Augustus Brennan <augustusmb@gmail.com>
2017-04-13 21:48:50 -0700

    Add route to enter topics-tags in their join table

--- server/routes/Tags_Topics.js
+++ server/routes/Tags_Topics.js
@@ -1,1 +1,46 @@
-Tags_Topics.js
+const db = require('./../../db/db.js');
+const path = require('path');
+const pg = require('pg');
+const pgp = require('pg-promise')();
+
+
+
+function sql(file) {
+  var fullPath = path.join(__dirname, './../../db/queries/tags_topics', file);
+  return new pgp.QueryFile(fullPath, {minify: true});
+}
+
+let queries = {
+  getTopicsByTag: sql('getTopicsByTag.sql'),
+  addTopicTag: sql('addTopicTag.sql')
+}
+
+
+module.exports.getTopicsByTag = (req, res) => {
+  const { tag_id } = req.query;
+
+  return db.query(queries.getTopicsByTag, [tag_id])
+  .then( data => {
+    console.log('Success getting topics by tag');
+    res.status(201).json(data);
+  })
+  .catch( error => {
+    res.status(404).send(error, 'FAILED getting topics by tag');
+  });
+}
+
+
+module.exports.addTopicTag = (req, res) => {
+  const { tag_id, topic_id } = req.body;
+
+  return db.query(queries.addTopicTag, [tag_id, topic_id])
+  .then(() => {
+    res.status(202).send('Success adding topic tag');
+  })
+  .catch( error => {
+    res.status(404).send('failed to add topic tag');
+  })
+}
+
+
+
