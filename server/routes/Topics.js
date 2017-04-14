aff5d697204b4cef3c4cb61e844d074516118409
Augustus Brennan <augustusmb@gmail.com>
2017-04-13 21:49:29 -0700

    Add a route to add a topic into the DB

--- server/routes/Topics.js
+++ server/routes/Topics.js
@@ -1,1 +1,30 @@
-Topics.js
+const db = require('./../../db/db.js');
+const path = require('path');
+const pg = require('pg');
+const pgp = require('pg-promise')();
+
+
+
+function sql(file) {
+  var fullPath = path.join(__dirname, './../../db/queries/topics', file);
+  return new pgp.QueryFile(fullPath, {minify: true});
+}
+
+let queries = {
+  addTopic: sql('insertTopic.sql'),
+}
+
+
+module.exports.addTopic = (req, res) => {
+  const { user_id, title, description, link, type } = req.body;
+
+  return db.query(queries.addTopic, [user_id, title, description, link, type])
+  .then( () => {
+    res.status(202).send('Success adding topic');
+  })
+  .catch( error => {
+    res.status(404).send('failed to add topic');
+  })
+}
+
+
