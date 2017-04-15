07c8ce0a92501cf4dcce733df159be84633bac11
Augustus Brennan <augustusmb@gmail.com>
2017-04-14 19:21:39 -0700

    get rid of articles in dummy data

--- db/dummyData.js
+++ db/dummyData.js
@@ -201,45 +201,6 @@ module.exports = {
     }
   ],
 
-  articles: [
-    {
-      author: 'Brian Heater',
-      title: 'Don’t run commercials designed to trigger smart assistants',
-      description: 'A well-known fast food chain – let’s call them Kurger Bing – is debuting a new 15 second ad today set to start running nationally. It starts off simply..',
-      url: 'https://techcrunch.com/2017/04/12/no-thank-you/',
-      url_to_image: 'https://tctechcrunch2011.files.wordpress.com/2017/04/img_2492.jpg?w=764&h=400&crop=1',
-      published_at: '2017-04-12T16:05:43Z',
-      source: 'techcrunch'
-    },
-    {
-      author: 'Josh Constine',
-      title: 'Facebook Messenger hits 1.2 billion monthly users, up from 1B in July',
-      description: 'Normally adding 200 million users in 8 months to a product that already has a billion would reduce the average engagement, but most apps don\'t have Facebook..',
-      url: 'https://techcrunch.com/2017/04/12/messenger/',
-      url_to_image: 'https://tctechcrunch2011.files.wordpress.com/2017/04/facebook-messenger-1-2-billion-users.png?w=764&h=400&crop=1',
-      published_at: '2017-04-12T16:00:33Z',
-      source: 'techcrunch'
-    },
-    {
-      author: 'Sarah Buhr',
-      title: 'Qualtrics waits on that IPO, raises $180 million at a $2.5 billion valuation instead',
-      description: 'That Qualtrics IPO many have been expecting is on hold for now. The online market research platform has just raised its third round for $180 million at a..',
-      url: 'https://techcrunch.com/2017/04/12/qualtrics-waits-on-that-ipo-raises-180-million-at-a-2-5-billion-valuation-instead/',
-      url_to_image: 'https://tctechcrunch2011.files.wordpress.com/2017/04/qualtrics_ryansmith-1_1024-e1491942393932.jpg?w=758&h=400&crop=1',
-      published_at: '2017-04-12T12:00:38Z',
-      source: 'techcrunch'
-    },
-    {
-      author: 'Sarah Perez',
-      title: 'Walmart to lower prices on a million online-only items if you opt for store pickup over shipping',
-      description: 'Walmart.com is preparing to launch a new feature called \'Pickup Discount\' that will lower the prices on up to one million online-only items if the customer..',
-      url: 'https://techcrunch.com/2017/04/11/walmart-to-lower-prices-on-a-million-online-only-items-if-you-opt-for-store-pickup-over-shipping/',
-      url_to_image: 'https://tctechcrunch2011.files.wordpress.com/2016/01/walmart-truckclose-up-side-view_129821854433586541.jpg?w=764&h=400&crop=1',
-      published_at: '2017-04-12T04:01:08Z',
-      source: 'techcrunch'
-    }
-  ],
-
   projects: [
     {
       id: 1,
