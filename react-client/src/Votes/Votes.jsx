e09275042633cd2d1d5fd7bbad69baf974a1f8ce
Paul Mills <paulmills@tests-MacBook-Pro-3.local>
2017-04-15 15:43:39 -0700

    (feat) Add different buttons for rendering

--- react-client/src/Votes/Votes.jsx
+++ react-client/src/Votes/Votes.jsx
@@ -4,10 +4,24 @@ class Vote extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      totalVotes: null,
+      userVote: null,
     };
   }
 
+  componentDidMount() {
+    const userVote = props.userVote || 0;
+    this.setState({
+      totalVotes: props.totalVotes,
+      userVote: userVote,
+    });
+  }
+
   render() {
+    const upVoteButton = <button className="btn btn-lg btn-block green-button" ><span className="glyphicon glyphicon-triangle-top" /></button>
+    const downVoteButton = <button className="btn btn-lg btn-block"><span className="glyphicon glyphicon-triangle-bottom" /></button>
+    const neutralUpVoteButton = <button className="btn btn-lg btn-block"><span className="glyphicon glyphicon-triangle-top" /></button>
+    const neutralDownVoteButton = <button className="btn btn-lg btn-block red-button"><span className="glyphicon glyphicon-triangle-bottom" /></button>
     return (
       <div className="text-center col-lg-1 col-md-1 col-sm-2 col-xs-2">
 
@@ -23,9 +37,9 @@ class Vote extends Component {
   }
 };
 
-const mapStateToProps = () => {
+const mapStateToProps = (state) => {
   return {
-
+    user: state.user
   }
 };
 
