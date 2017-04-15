5d9b4b217a2a18b253883a32722ce1204719b67b
Augustus Brennan <augustusmb@gmail.com>
2017-04-14 19:00:10 -0700

    Add a type column, and remove topic_id foreign key constraint

--- db/init/tables/comments.sql
+++ db/init/tables/comments.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS comments (
   id serial PRIMARY KEY,
   user_id int,
   created_at timestamp DEFAULT current_timestamp,
+  type text,
   topic_id int,
   content text,
-  FOREIGN KEY(user_id) REFERENCES users,
-  FOREIGN KEY(topic_id) REFERENCES topics
+  FOREIGN KEY(user_id) REFERENCES users
 );
