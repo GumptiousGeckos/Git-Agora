02321771269ff06bc0b4295985607eac5f1cffd2
Paul Mills <paulmills@tests-MacBook-Pro-3.local>
2017-04-17 16:59:39 -0700

    (feat) Add query to return articles by votes

---
+++ db/queries/articles/getAllArticles.sql
@@ -0,0 +1,1 @@
+select articlestable.*, votestable.votes from (select * from articles) articlestable left join (select sum(vote_type) votes, topic_id from votes where type='article' group by topic_id) votestable on (articlestable.id = votestable.topic_id) order by votestable.votes desc;
