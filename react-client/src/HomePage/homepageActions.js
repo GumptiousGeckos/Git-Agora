571bd172fcd91e97c453ca7b1d62d37440e82494
Paul Mills <paulmills@tests-MacBook-Pro-3.local>
2017-04-17 14:51:59 -0700

    (fix) Remove hot news dummy data

--- react-client/src/HomePage/homepageActions.js
+++ react-client/src/HomePage/homepageActions.js
@@ -1,80 +1,5 @@
 import axios from 'axios';
 
-const hotNewsDummyData = [
-  {
-    id: 0,
-    author: 'Brian Heater',
-    title: 'Don’t run commercials designed to trigger smart assistants',
-    description: 'A well-known fast food chain – let’s call them Kurger Bing – is debuting a new 15 second ad today set to start running nationally. It starts off simply..',
-    url: 'https://techcrunch.com/2017/04/12/no-thank-you/',
-    urlToImage: 'https://tctechcrunch2011.files.wordpress.com/2017/04/img_2492.jpg?w=764&h=400&crop=1',
-    publishedAt: '2017-04-12T16:05:43Z',
-    source: 'techcrunch',
-    likes: 23,
-    dislikes: 0
-  },
-  {
-    id: 1,
-    author: 'Josh Constine',
-    title: 'Facebook Messenger hits 1.2 billion monthly users, up from 1B in July',
-    description: 'Normally adding 200 million users in 8 months to a product that already has a billion would reduce the average engagement, but most apps don\'t have Facebook..',
-    url: 'https://techcrunch.com/2017/04/12/messenger/',
-    urlToImage: 'https://tctechcrunch2011.files.wordpress.com/2017/04/facebook-messenger-1-2-billion-users.png?w=764&h=400&crop=1',
-    publishedAt: '2017-04-12T16:00:33Z',
-    source: 'techcrunch',
-    likes: 7,
-    dislikes: 2
-  },
-  {
-    id: 2,
-    author: 'Sarah Buhr',
-    title: 'Qualtrics waits on that IPO, raises $180 million at a $2.5 billion valuation instead',
-    description: 'That Qualtrics IPO many have been expecting is on hold for now. The online market research platform has just raised its third round for $180 million at a..',
-    url: 'https://techcrunch.com/2017/04/12/qualtrics-waits-on-that-ipo-raises-180-million-at-a-2-5-billion-valuation-instead/',
-    urlToImage: 'https://tctechcrunch2011.files.wordpress.com/2017/04/qualtrics_ryansmith-1_1024-e1491942393932.jpg?w=758&h=400&crop=1',
-    publishedAt: '2017-04-12T12:00:38Z',
-    source: 'techcrunch',
-    likes: 11,
-    dislikes: 4
-  },
-  {
-    id: 3,
-    author: 'Brian Heater',
-    title: 'Don’t run commercials designed to trigger smart assistants',
-    description: 'A well-known fast food chain – let’s call them Kurger Bing – is debuting a new 15 second ad today set to start running nationally. It starts off simply..',
-    url: 'https://techcrunch.com/2017/04/12/no-thank-you/',
-    urlToImage: 'https://tctechcrunch2011.files.wordpress.com/2017/04/img_2492.jpg?w=764&h=400&crop=1',
-    publishedAt: '2017-04-12T16:05:43Z',
-    source: 'techcrunch',
-    likes: 23,
-    dislikes: 0
-  },
-  {
-    id: 4,
-    author: 'Josh Constine',
-    title: 'Facebook Messenger hits 1.2 billion monthly users, up from 1B in July',
-    description: 'Normally adding 200 million users in 8 months to a product that already has a billion would reduce the average engagement, but most apps don\'t have Facebook..',
-    url: 'https://techcrunch.com/2017/04/12/messenger/',
-    urlToImage: 'https://tctechcrunch2011.files.wordpress.com/2017/04/facebook-messenger-1-2-billion-users.png?w=764&h=400&crop=1',
-    publishedAt: '2017-04-12T16:00:33Z',
-    source: 'techcrunch',
-    likes: 7,
-    dislikes: 2
-  },
-  {
-    id: 5,
-    author: 'Sarah Buhr',
-    title: 'Qualtrics waits on that IPO, raises $180 million at a $2.5 billion valuation instead',
-    description: 'That Qualtrics IPO many have been expecting is on hold for now. The online market research platform has just raised its third round for $180 million at a..',
-    url: 'https://techcrunch.com/2017/04/12/qualtrics-waits-on-that-ipo-raises-180-million-at-a-2-5-billion-valuation-instead/',
-    urlToImage: 'https://tctechcrunch2011.files.wordpress.com/2017/04/qualtrics_ryansmith-1_1024-e1491942393932.jpg?w=758&h=400&crop=1',
-    publishedAt: '2017-04-12T12:00:38Z',
-    source: 'techcrunch',
-    likes: 11,
-    dislikes: 4
-  }
-];
-
 export const requestHotProjects = () => ({
   type: 'FETCHING_HOT_PROJECTS'
 });
@@ -126,8 +51,5 @@ export const fetchHotNews = () => (
       .catch((err) => {
         dispatch(errorHotNews(err));
       });
-    // setTimeout(() => {
-    //   dispatch(receivedHotNews(hotNewsDummyData));
-    // }, 1000);
   }
 );
