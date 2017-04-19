95eccc40e3c05475b1c32934eeff3923ba4b3467
Augustus Brennan <augustusmb@gmail.com>
2017-04-19 16:21:56 -0700

    Change layout of commentEntry slightly.  Render the values of the comment

--- react-client/src/CommentSection/CommentEntry.jsx
+++ react-client/src/CommentSection/CommentEntry.jsx
@@ -12,22 +12,23 @@ export default (props) => {
 
 
   return (
-    <div>
+    <div className="comment">
       <div className="row">
-        <div className="col-md-2">
+        <div className="col-md-1">
         Post #1
         </div>
-        <div className="col-md-10">
-          {props.comment.user_id}
+        <div className="col-md-11">
+          <span id="username">{props.comment.user_id}</span>
+          <span className="timestamp">{props.comment.created_at}</span>
         </div>
       </div>
       <div className="row">
-        <div className="col-md-2">
+        <div className="col-md-1">
           <div >Dev: 250</div>
           <div >Idea: 350</div>
 
         </div>
-        <div className="col-md-10">
+        <div className="col-md-11">
           <p>{props.comment.content}   </p>
         </div>
       </div>
