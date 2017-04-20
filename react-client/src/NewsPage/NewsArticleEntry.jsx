0cadfc241af2ff12a5306fc2c9e2cc6f84d927ab
Paul Mills <paulmills@tests-MacBook-Pro-3.local>
2017-04-19 21:41:55 -0700

    (feat) Update newsArticleEntry to pass required props to Votes

--- react-client/src/NewsPage/NewsArticleEntry.jsx
+++ react-client/src/NewsPage/NewsArticleEntry.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Votes from './Votes.jsx';
+import Votes from '../Votes/Votes.jsx';
 
 export default (props) => {
-  let { votes, title, url, source, description } = props.article;
+  let { votes, title, url, source, description, vote_type, id } = props.article;
   return (
     <div>
-      <Votes votes={votes} />
+      <Votes votes={votes} topic_type={'article'} vote_type={vote_type} topic_id={id}/>
       <div className="well col-lg-11 col-md-11 col-sm-10 col-xs-10">
         <a href={url}>
           <h4>{title}</h4>
