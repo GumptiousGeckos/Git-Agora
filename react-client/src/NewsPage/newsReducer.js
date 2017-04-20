f05d92c20006c05190521f6ed95787185999d6c9
Paul Mills <paulmills@tests-MacBook-Pro-3.local>
2017-04-19 21:43:05 -0700

    (feat) Write TOGGLE_ARTICLE_VOTE case to update db and state on vote

--- react-client/src/NewsPage/newsReducer.js
+++ react-client/src/NewsPage/newsReducer.js
@@ -4,7 +4,7 @@ const initialState = {
 };
 
 
-const articles = (state = initialState, action) => {
+const news = (state = initialState, action) => {
   switch (action.type) {
     case 'FETCHING_ARTICLES':
       return {
@@ -23,9 +23,33 @@ const articles = (state = initialState, action) => {
         error: action.payload,
         fetchingArticles: false
       };
+    case 'TOGGLE_ARTICLE_VOTE': 
+      return {
+        ...state,
+        articles: state.articles.map((article) => {
+          if (article.id === action.topic_id) {
+            article.vote_type = article.vote_type || 0;
+            if (article.vote_type === -1 ) {
+              article.votes = (Number(article.votes) + 1).toString();
+              article.vote_type = 0;
+              return article;
+            } else if (article.vote_type === 1) {
+              article.votes = (Number(article.votes) - 1).toString();
+              article.vote_type = 0;
+              return article;
+            } else if (article.vote_type === 0) {
+              article.votes = (Number(article.votes) + action.vote_type).toString();
+              article.vote_type = action.vote_type;
+              return article;
+            }
+          } else {
+            return article;
+          }
+        })
+      }
     default:
       return state;
   }
 };
 
-export default articles;
+export default news;
