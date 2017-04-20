3a3959c023c8b6a806fe6476c3510ccbf6c4ac88
Paul Mills <paulmills@tests-MacBook-Pro-3.local>
2017-04-20 12:27:02 -0700

    (feat) Update updateVotes to insert a vote if it doesn't exist

--- server/routes/Votes.js
+++ server/routes/Votes.js
@@ -13,7 +13,8 @@ function sql(file) {
 let queries = {
   addVote: sql('insertVote.sql'),
   updateVote: sql('updateVoteValue.sql'),
-  getVotes: sql('getVoteCount.sql')
+  getVotes: sql('getVoteCount.sql'),
+  checkForVote: sql('getVoteByUserIdTopicIdAndType.sql')
 }
 
 
@@ -30,29 +31,42 @@ module.exports.addVote = (req, res) => {
 }
 
 
-module.exports.updateVote = (req, res) => {
-  const { vote_type, user_id, type, topic_id } = req.body;
-
-  return db.query(queries.updateVote, [vote_type, user_id, type, topic_id])
-  .then( () => {
-    res.status(204).send('Success updating vote');
-  })
-  .catch( error => {
-    res.status(404).send('Error updating vote');
-  })
-}
-
 module.exports.getVotes = (req, res) => {
   const { type, topic_id } = req.query;
 
   return db.query(queries.getVotes, [type, topic_id])
    .then( data => {
-    console.log('Success getting votes');
     res.status(200).json(data);
   })
   .catch( error => {
-    console.log(error);
     res.status(404).send('Error getting votes');
+  })
+}
+
+module.exports.updateVote = (req, res) => {
+  const { vote_type, user_id, type, topic_id } = req.body;
+  return db.query(queries.checkForVote, {user_id: user_id, vote_type: vote_type, type: type, topic_id: topic_id})
+  .then( (result) => {
+    if (result[0]) {
+      return db.query(queries.updateVote, {user_id: user_id, vote_type: vote_type, type: type, topic_id: topic_id})
+      .then( () => {
+        res.status(204).send('Success updating vote');
+      })
+      .catch( error => {
+        res.status(404).send('Error updating vote');
+      })
+    } else {
+      return db.query(queries.addVote, {user_id: user_id, vote_type: vote_type, type: type, topic_id: topic_id})
+      .then( () => {
+        res.status(201).send('Success adding vote');
+      })
+      .catch( error => {
+        res.status(404).send('Error adding vote');
+      })
+    }
+  })
+  .catch( error => {
+    res.status(404).send('Error updating vote');
   })
 }
 
