34b2258aa15f2ac9a453e9786468b43a262692bf
Paul Mills <paulmills@tests-MacBook-Pro-3.local>
2017-04-20 12:31:11 -0700

    (feat) Add TOGGLE_PROJECT_VOTE case to update state on user vote

--- react-client/src/ProjectIdeas/projectReducer.js
+++ react-client/src/ProjectIdeas/projectReducer.js
@@ -41,6 +41,30 @@ const projects = (state = {}, action) => {
         ...state,
         mainProject: action.payload
       };
+    case 'TOGGLE_PROJECT_VOTE': 
+      return {
+        ...state,
+        projects: state.projects.map((project) => {
+          if (project.id === action.topic_id) {
+            project.vote_type = project.vote_type || 0;
+            if (project.vote_type === -1 ) {
+              project.votes = (Number(project.votes) + 1).toString();
+              project.vote_type = 0;
+              return project;
+            } else if (project.vote_type === 1) {
+              project.votes = (Number(project.votes) - 1).toString();
+              project.vote_type = 0;
+              return project;
+            } else if (project.vote_type === 0) {
+              project.votes = (Number(project.votes) + action.vote_type).toString();
+              project.vote_type = action.vote_type;
+              return project;
+            }
+          } else {
+            return project;
+          }
+        })
+      };
     default:
       return state;
   }
