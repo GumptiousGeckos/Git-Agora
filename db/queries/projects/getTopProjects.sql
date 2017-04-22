03f7c8cda88a36baa662e85778d0fd41238e0bad
Jonathan Chou <jonchou@ucdavis.edu>
2017-04-22 15:47:04 -0700

    (feat) projectIdeas page shows tags

--- db/queries/projects/getTopProjects.sql
+++ db/queries/projects/getTopProjects.sql
@@ -1,1 +1,41 @@
-select projectstable.*, votestable.votes from (select sum(vote_type) votes, topic_id from votes where type='project' group by topic_id) votestable left join (select * from projects) projectstable on (projectstable.id = votestable.topic_id) order by votestable.votes desc limit 25;
+-- combines projects, votecount, and tags (as an aggregated string)
+select
+  projectstable.*, votestable.votes
+from
+  (select
+    sum(vote_type) votes, topic_id
+  from
+    votes
+  where
+    type='project'
+  group by
+    topic_id
+  ) votestable
+left join
+  (select
+    proj.*, tags.tags
+  FROM
+    (select
+      *
+    from
+      projects
+    ) proj
+  LEFT JOIN
+    (select
+      pt.project_id, array_to_string(array_agg(t.tag_name), ',') as tags
+    from
+      projects_tags pt
+    left join
+      tags t
+    on
+      (pt.tag_id = t.id)
+    group by
+      pt.project_id
+    ) tags
+  on
+    (proj.id = tags.project_id)
+  ) projectstable
+on
+  (projectstable.id = votestable.topic_id)
+order by
+  votestable.votes desc limit 25;
--- react-client/src/ProjectIdeas/ProjectBoardEntry.jsx
+++ react-client/src/ProjectIdeas/ProjectBoardEntry.jsx
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom';
 import Votes from '../Votes/Votes.jsx';
 
 export default (props) => {
-  let { votes, title, url, source, description, vote_type, id } = props.project;
+  let { votes, title, url, source, description, vote_type, id, tags } = props.project;
+  const tagArray = tags ? tags.split(',') : [];
   return (
   <div>
     <Votes votes={votes} topic_type={'project'} vote_type={vote_type} topic_id={id}/>
@@ -12,6 +13,13 @@ export default (props) => {
         className="well col-lg-11 col-md-11 col-sm-10 col-xs-10"
       >
         <h4>{title}</h4>
+        <h6>Tags:
+          {
+            tagArray.map(tag =>
+              <span>{' ' + tag}</span>
+            )
+          }
+        </h6>
         <h4 className="standard">{description}</h4>
       </div>
     </Link>
