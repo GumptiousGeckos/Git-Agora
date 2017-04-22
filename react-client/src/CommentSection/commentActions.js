48199cdd4b4d49dfb760c6de0be92702e752fd33
Augustus Brennan <augustusmb@gmail.com>
2017-04-22 14:23:22 -0700

    Change user_id to username.  Include avatar in payload

--- react-client/src/CommentSection/commentActions.js
+++ react-client/src/CommentSection/commentActions.js
@@ -4,9 +4,9 @@ export const getComments = () => ({
   type: 'FETCHING_COMMENTS'
 });
 
-export const addComment = (topic_id, type, user_id, date_created, content) => ({
+export const addComment = (topic_id, type, username, date_created, content, avatar) => ({
   type: 'ADD_COMMENT',
-  payload: { topic_id, type, user_id, date_created, content }
+  payload: { topic_id, type, username, date_created, content, avatar }
 });
 
 export const receivedComments = comments => ({
@@ -37,13 +37,12 @@ export const fetchComments = (topic_id, type) => (
   }
 );
 
-export const insertComment = (topic_id, type, user_id, date_created, content) => (
+export const insertComment = (topic_id, type, username, date_created, content, avatar) => (
   (dispatch) => {
-    dispatch(addComment(topic_id, type, user_id, date_created, content));
-    axios.post('/api/comments', { topic_id, type, user_id, date_created, content })
+    axios.post('/api/comments', { topic_id, type, username, date_created, content })
       .then((response) => {
+        dispatch(addComment(topic_id, type, username, date_created, content, avatar));
         console.log('Comment added');
-        // dispatch(fetchComments(topic_id, type));
       })
       .catch((err) => {
         console.log('ERROR adding comment: ', err);
