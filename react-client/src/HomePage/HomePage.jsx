7c3b1264ad72100f3820e749d63347aebeee86a9
Paul Mills <paulmills@tests-MacBook-Pro-3.local>
2017-04-26 19:43:44 -0700

    (feat) Refactor homepage to use carousel and project card components

--- react-client/src/HomePage/HomePage.jsx
+++ react-client/src/HomePage/HomePage.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchHotProjects, fetchHotNews } from './homepageActions';
+import ImageGallery from 'react-image-gallery';
+import NewsImageGallery from './NewsImageGallery.jsx';
+import HomePageProjectEntry from './HomePageProjectEntry.jsx';
+
 
 
 export class HomePage extends React.Component {
@@ -16,46 +20,30 @@ export class HomePage extends React.Component {
     const { hotProjects, hotNews} = this.props;
 
     return (
+      <div>
       <div className="container">
-        <div className="text-center">
-          <h1>Trending Tech News</h1>
+        <div className="row news-image-gallery-row">
+          <NewsImageGallery hotNews={hotNews} />
         </div>
-        <div className="row news-border">
-          {
-            hotNews.map(article =>
-              <div className="col-md-4 front-page" key={article.id}>
-                <div className="thumbnail">
-                  <a href={article.url}>
-                    <h4 className="title">{article.title}</h4>
-                    <img src={article.url_to_image} />
-                    <div className="caption text-center">
-                      <p>{article.description}</p>
-                    </div>
-                  </a>
-                </div>
-              </div>
-            )
-          }
+      </div>
+      <div className="container">
+      <div className="row">
+        <div className="twelve columns">
+          <div className="projects-header">Hot Projects</div>
         </div>
-
-        <div className="text-center col-md-12">
-          <h1>Hot Projects</h1>
+      </div>
+        <div className="row">
+          <div className="top-three-projects four columns ">
+          {hotProjects.slice(0, 3).map(project => <HomePageProjectEntry project={project}/>)}
+          </div>
+          <div className="top-three-projects four columns ">
+          {hotProjects.slice(3, 6).map(project => <HomePageProjectEntry project={project}/>)}
+          </div>
+          <div className="top-three-projects four columns ">
+          {hotProjects.slice(6, 9).map(project => <HomePageProjectEntry project={project}/>)}
+          </div>
         </div>
-        <div className="list-group">
-          {
-            hotProjects.map(project =>
-              <Link to={'/projects/' + project.id} key={project.id}>
-                <button
-                  type="button"
-                  className="list-group-item"
-                >
-                  <span>{project.title}</span>
-                  <h4>{project.description}</h4>
-                </button>
-              </Link>
-            )
-          }
-        </div>
+      </div>
       </div>
     );
   }
