22b3427fb455d71c9f3b1a7e0740015466f9928d
Augustus Brennan <augustusmb@gmail.com>
2017-04-26 22:09:11 -0700

    Make userProfile a class component, format userProfile page

--- react-client/src/Users/UserProfile.jsx
+++ react-client/src/Users/UserProfile.jsx
@@ -1,20 +1,81 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 
 import UserProjects from './UserProjects.jsx';
 import UserDetails from './UserDetails.jsx';
 import UserFavorites from '../Favorites/UserFavorites.jsx';
 
-export default (props) => {
-  const { id } = props.match.params;
-  return (
-    <div className="container">
-      <div className="col-md-4">
-        <UserDetails id={id} />
-        <UserFavorites />
+export class UserProfile extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+
+    const { id } = this.props.match.params;
+    this.state = {
+      renderThis: <UserProjects id={id} />
+    }
+  }
+
+  componentWillMounts() {
+  }
+
+  handleClick(e) {
+    const { id } = this.props.match.params;
+    let toRender;
+
+    if (e.target.value === 'projects') {
+      toRender = <UserProjects id={id} />;
+    } else if (e.target.value === 'favorites') {
+      toRender = <UserFavorites />;
+    } else {
+      toRender = <h4 className="tab-title">Contributions</h4>;
+    }
+
+    this.setState({ renderThis: toRender })
+  }
+
+  render() {
+    const { id } = this.props.match.params;
+    const { renderThis } = this.state;
+
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="four columns profile-details">
+            <UserDetails id={id} />
+          </div>
+          <div className="eight columns profile-tab-area">
+            <table className="u-full-width">
+              <thead>
+                <tr>
+                  <th><button className="profile-tabs" value="projects" onClick={this.handleClick}>Projects</button></th>
+                  <th><button className="profile-tabs" value="favorites" onClick={this.handleClick}>Favorites</button></th>
+                  <th><button className="profile-tabs" value="contributions" onClick={this.handleClick}>Contributions</button></th>
+                </tr>
+              </thead>
+            </table>
+            <div className="container">
+              {renderThis}
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="col-md-8">
-        <UserProjects id={id} />
-      </div>
-    </div>
-  );
+    );
+  }
 };
+
+const mapStateToProps = (state) => {
+  return {
+
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserProfile);
