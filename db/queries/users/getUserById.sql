b69c8c3f31f383b7dcd6b83bd197ca0f5e7e54f7
Edward Chan <edwardchan@Edwards-MacBook-Pro.local>
2017-04-27 21:09:17 -0700

    (fix) fixes query to get dev and idea points for our user profile page

--- db/queries/users/getUserById.sql
+++ db/queries/users/getUserById.sql
@@ -1,10 +1,10 @@
 SELECT
-  users.*, ratings.idea_points, ratings.dev_points
+  users.*, contributions.idea_points, contributions.dev_points
 FROM
   users
 LEFT JOIN
   ratings
 ON
-  users.id = ratings.user_id
+  users.id = contributions.user_id
 where
   users.id = ${id}
