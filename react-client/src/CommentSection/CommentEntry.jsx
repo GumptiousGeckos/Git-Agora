9ecc58914fe0bd2bcbc11eafd3ee7464e58b98d8
Jonathan Chou <jonchou@ucdavis.edu>
2017-04-27 20:16:40 -0700

    (style) restyle comments and add in TimeAgo

--- react-client/src/CommentSection/CommentEntry.jsx
+++ react-client/src/CommentSection/CommentEntry.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import TimeAgo from 'react-timeago';
 
 
 export default (props) => {
@@ -7,14 +8,16 @@ export default (props) => {
   return (
     <div className="comment">
       <div className="row">
-        <div className= "three columns comment-meta">
-            <Link to={'/users/' + comment.user_id}>
-              <img src={comment.avatar} className="comment-avatar" alt="Responsive image"/>
-           </Link>
-            <Link to={'/users/' + comment.user_id}>
-              <span className="comment-username">{comment.username}</span>
-            </Link>
-          <div className="date-created">{comment.date_created}</div>
+        <div className="three columns comment-meta">
+          <Link to={'/users/' + comment.user_id}>
+            <img src={comment.avatar} className="comment-avatar" alt="Responsive image"/>
+          </Link>
+          <Link to={'/users/' + comment.user_id}>
+            <span className="comment-username">{comment.username}</span>
+          </Link>
+          <div className="date-created">
+            <TimeAgo date={comment.date_created} />
+          </div>
         </div>
         <div className="nine columns content-section">
           <p className="comment-content">{comment.content}</p>
